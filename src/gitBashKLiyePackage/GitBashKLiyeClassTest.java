e45b63336f1e3c3786c07896402c34ad19d8435d
m-kamaal <m.kamaal1996@gmail.com>
2021-10-29 12:36:15 +0530

    second commit kr rha hu

--- src/gitBashKLiyePackage/GitBashKLiyeClassTest.java
+++ src/gitBashKLiyePackage/GitBashKLiyeClassTest.java
@@ -5,6 +5,7 @@ public class GitBashKLiyeClassTest {
 	public static void main(String[] args) {
 
        System.out.println("Aur vai kya haal?");
+       System.out.println("Sab vadia vai");
 		
 
 	}
