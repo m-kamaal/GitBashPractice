fdafc2dc34a49ee4193601c6ea7710f02f2ac0c4
m-kamaal <m.kamaal1996@gmail.com>
2021-10-29 13:09:50 +0530

    sdddddd

--- src/gitBashKLiyePackage/PracticeClass1Test.java
+++ src/gitBashKLiyePackage/PracticeClass1Test.java
@@ -1,12 +1,13 @@
 package gitBashKLiyePackage;
 
-public class GitBashKLiyeClassTest {
+public class PracticeClass1Test {
 
 	public static void main(String[] args) {
 
        System.out.println("Aur vai kya haal?");
        System.out.println("Sab vadia vai");
        System.out.println("Hello brother");
+       System.out.println("Here we go again");
 		
 
 	}
---
+++ src/gitBashKLiyePackage/PracticeClass2Test.java
@@ -0,0 +1,10 @@
+package gitBashKLiyePackage;
+
+public class PracticeClass2Test {
+
+	public static void main(String[] args) {
+		System.out.println("Lets practice here now");
+
+	}
+
+}
